5a85f1bcf40d12ff1fba1f4fc6a2e7aa6f67bd4a
seanseow123 <seanseow@Seans-MacBook-Air.local>
2022-10-14 16:20:24 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+
+//Question 15
+class Car {
+    constructor(name, brand, color, manufacture) {
+        this.name = name;
+        this.brand = brand;
+        this.color = color;
+        this.manu = manufacture;
+    }
+}
+
+const City = new City();
+const specialFeature = new specialFeature();
+
+
+//Question 16
+class Book {
+    constructor(numOfPages, typeOfPages, author) {
+        this.numOfPages = numOfPages;
+        this.typeOfPages = typeOfPages;
+        this.author = author;
+    }
+}
+
+const type_of_book = new type_of_book();
+
+
+//Question 17
+class Animal {
+    constructor(gender, name, disease) {
+        this.gender;
+        this.name;
+        this.disease;
+    }
+}
+
+const walk = new walk();
+const eat = new eat();
+const climb = new climb();
+
+
+
+
